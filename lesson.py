f12f5d4bebfdc3a462bed63a6f85fb75934daa8b
Sumio K <tap.miwo@gmail.com>
2022-11-22 06:23:18 +0900

    first commit

---
+++ lesson.py
@@ -0,0 +1,14 @@
+num = 1
+name = "mike"
+is_ok = True
+
+num = name
+print(num, type(num))
+print(name)
+print(is_ok, type(is_ok))
+
+num = 1
+name = "1"
+
+new_num = int(name)
+print(new_num, type(new_num))
