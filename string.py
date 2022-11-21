587d1064a61b1700804bb4bf2f12198acaaae0c6
Sumio K <tap.miwo@gmail.com>
2022-11-22 06:44:30 +0900

    done string

---
+++ string.py
@@ -0,0 +1,25 @@
+s = "test"
+print(s)
+print("I don't know")
+print('I don\'t know')
+print('say "I don\'t know"')
+print("say \"i don't know\"")
+
+print("hello. \nHow are you")
+print(r"C:\name\name")
+
+# 自動改行
+print("##########################")
+print("""\
+line1
+line2
+line3\
+""")
+print("##########################")
+
+print("Hi." * 3 + "Mike")
+
+# 長い文章を二行に分けて書く
+s = ('aaaaaaaaaaaaaaaaaaaaaaaaaaa'
+      'bbbbbbbbbbbbbbbbbbbbbbbbbb')
+print(s)
