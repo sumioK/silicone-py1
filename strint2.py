9447d4c358f45274f2a19abf33b0387658430e87
Sumio K <tap.miwo@gmail.com>
2022-11-23 06:21:27 +0900

    string def

---
+++ strint2.py
@@ -0,0 +1,26 @@
+print('a is {}{}{}'.format('test',2,'hey'))
+print('My name is {0}{1}'.format('Sumio','Kawaguchi'))
+
+print('My name is {name} {family}. watashi ha {family} {name} desu.'.format(name='Sumio', family='Kawaguchi'))
+
+print('#######################################################')
+a = 'a'
+print(f'a is {a}')
+
+x, y, z = 1, 2, 3
+print(f'a is {x}, {y}, {z}')
+
+name = 'Jun'
+family = 'Sakai'
+print(f'My name is {name} {family}')
+
+print('#######################################################')
+
+
+x = str(1)
+print(type(x))
+y = str(3.14)
+print(type(y))
+z = str(True)
+print(type(z))
+
