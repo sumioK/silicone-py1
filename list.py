1aef7023b0134aafc98c6c2b9da32b6075d310f1
Sumio K <tap.miwo@gmail.com>
2022-11-23 06:37:27 +0900

    list

---
+++ list.py
@@ -0,0 +1,26 @@
+l = [1, 20, 4, 50, 2, 1, 2]
+print(l)
+print(l[0])
+print(l[-1])
+print(l[0:2])
+print(l[:2])
+print(l[3:])
+print(len(l))
+
+print(type(l))
+print(list('abcdefg'))
+
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(n[::2])
+print(n[::-1])
+
+print('##############################################')
+
+
+a = ['a', 'b', 'c']
+n = [1, 2, 3]
+x = [a, n]
+print(x)
+print(x[0])
+print(x[1])
+print(x[0][1])
