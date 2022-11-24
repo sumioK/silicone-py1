1e948cbc282f05d810ee57a4144dec0eb8f71a00
Sumio K <tap.miwo@gmail.com>
2022-11-25 06:46:15 +0900

    set

---
+++ lesson2/set.py
@@ -0,0 +1,12 @@
+a = {1, 2, 2, 3, 4, 4, 4, 5, 6}
+print(a)
+print(type(a))
+
+print('################################')
+b = {2, 3, 3, 6, 7}
+print('a=',a)
+print('b=',b)
+print(a-b)
+print(a & b)
+print(a | b)
+print(a ^ b)
