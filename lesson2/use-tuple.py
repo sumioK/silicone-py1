a01fcdbef21c3a0fbbc9acbc0609f37ef62e4263
Sumio K <tap.miwo@gmail.com>
2022-11-24 20:57:08 +0900

    use tuple

---
+++ lesson2/use-tuple.py
@@ -0,0 +1,10 @@
+chose_from_two = ('A', 'B', 'C')
+answer = []
+answer.append('A')
+answer.append('C')
+
+# 間違えて問題文に答えを追加してしまった場合にtupleではエラーとなる
+# chose_from_two.append('A')
+# chose_from_two.append('C')
+print(chose_from_two)
+print(answer)
