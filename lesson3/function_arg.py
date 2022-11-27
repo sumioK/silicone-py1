20d853b38961af5839701d22a936650de33e04e2
Sumio K <tap.miwo@gmail.com>
2022-11-28 06:40:46 +0900

    function argument attention

--- lesson3/function_arg.py
+++ lesson3/function_arg.py
@@ -14,3 +14,34 @@ def menu(entree='beef', drink='whine' ,dessert='ice'):
   print(dessert)
 
 menu(entree = 'checken')
+
+
+print('########################################################')
+def test_func(x, l=[]):
+  l.append(x)
+  return l
+
+y = [1, 2, 3]
+r = test_func(100, y)
+print(y)
+
+# 100を2回渡されると100が2買い渡されてしまう。
+# default引数で関数を使用すべきではない
+r = test_func(100)
+print(r)
+r = test_func(100)
+print(r)
+
+
+print('########################################################')
+def test_func(x, l =None):
+  # 関数の中で空のリストを渡せば解決できる
+  if l is None:
+    l = []
+  l.append(x)
+  return l
+
+r = test_func(100)
+print(r)
+r = test_func(100)
+print(r)
