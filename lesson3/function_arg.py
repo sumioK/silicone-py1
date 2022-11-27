15d8d13b74161e43db69b1b9e0ff1c198db1c0ad
Sumio K <tap.miwo@gmail.com>
2022-11-28 06:31:12 +0900

    function argument

---
+++ lesson3/function_arg.py
@@ -0,0 +1,16 @@
+def menu(entree, drink, dessert):
+  print(entree)
+  print(drink)
+  print(dessert)
+
+# 順番または引数が正しくないといけない
+menu(entree='beef', drink='beer', dessert='dessert')
+
+
+print('########################################################')
+def menu(entree='beef', drink='whine' ,dessert='ice'):
+  print(entree)
+  print(drink)
+  print(dessert)
+
+menu(entree = 'checken')
