b3f3911b2e98b04689a2db00375affa5a478372f
Sumio K <tap.miwo@gmail.com>
2022-11-27 16:39:08 +0900

    input

---
+++ lesson3/input.py
@@ -0,0 +1,13 @@
+while True:
+  word = input('Enter string:')
+  if word == 'ok':
+    break
+  print('next')
+
+
+while True:
+  word = input('Enter Number:')
+  num = int(word)
+  if num == 100:
+    break
+    print('next')
