703950af4475e22d779b74b55d6d01a9b31998cf
Sumio K <tap.miwo@gmail.com>
2022-11-28 06:07:55 +0900

    function return

---
+++ lesson3/function.py
@@ -0,0 +1,18 @@
+def say_something():
+  s = 'hi'
+  return s
+
+result = say_something()
+print(result)
+
+
+
+def what_is_this(color):
+  if color == 'red':
+    return 'tomato'
+  else:
+    return color
+
+result2 = what_is_this('red')
+print(result2)
+
