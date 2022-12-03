229bde1ad715e13090b0f04f5e01f29220185240
Sumio K <tap.miwo@gmail.com>
2022-12-03 11:34:39 +0900

    import error

--- lesson4/lesson.py
+++ lesson4/lesson.py
@@ -1,11 +1,17 @@
-from lesson_package import utils # as で名前を指定できる
-from lesson_package.talk import *
-r = utils.say_twice('hello')
-print(r)
+# from lesson_package import utils # as で名前を指定できる
+# from lesson_package.talk import *
+# r = utils.say_twice('hello')
+# print(r)
 
-print(human.sing())
-print(human.cry())
+# print(human.sing())
+# print(human.cry())
 
 
-print(animal.sing())
-print(animal.cry())
+# print(animal.sing())
+# print(animal.cry())
+try:
+  from lesson_package import utils
+except ImportError:
+  from lesson_package.tools import utils
+
+utils.say_twice('word')
