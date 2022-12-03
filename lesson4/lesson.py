e4eaf0ecdbef712842b4afbf9e5bbe9556c012be
Sumio K <tap.miwo@gmail.com>
2022-12-03 11:31:34 +0900

    __all__ import

--- lesson4/lesson.py
+++ lesson4/lesson.py
@@ -1,7 +1,11 @@
 from lesson_package import utils # as で名前を指定できる
-from lesson_package.talk import human
+from lesson_package.talk import *
 r = utils.say_twice('hello')
 print(r)
 
 print(human.sing())
 print(human.cry())
+
+
+print(animal.sing())
+print(animal.cry())
---
+++ lesson4/lesson_package/talk/animal.py
@@ -0,0 +1,7 @@
+from lesson_package import utils 
+
+def sing():
+  return 'lkjgdkl;asldkjfgkl;'
+
+def cry():
+  return utils.say_twice('dk,xcieoengkeokd')
