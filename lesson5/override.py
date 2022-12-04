ee07880f2ab603fe91066de2fdcdfef00ab11c86
Sumio K <tap.miwo@gmail.com>
2022-12-04 19:08:07 +0900

    super, override

---
+++ lesson5/override.py
@@ -0,0 +1,39 @@
+class Car(object):
+  def __init__(self, model=None):
+    self.model = model
+
+  def run(self):
+    print('run')
+
+class ToyotaCar(Car):
+  def run(self):
+    print('run fast')
+
+class TeslaCar(Car):
+  def __init__(self, model='Model S', enable_auto_run=False):
+    # self.model = model
+    # super()で親のクラス関数を呼べる
+    super().__init__(model)
+    self.enable_auto_run = enable_auto_run
+  def run(self):
+    print('run super fast')
+  def  auto_run(self):
+    print('auto_run')
+
+
+car = Car()
+car.run()
+
+print('#############################################')
+
+toyota_car = ToyotaCar('Lexus')
+print(toyota_car.model)
+toyota_car.run()
+
+print('#############################################')
+
+tesla_car = TeslaCar('Model S')
+print(tesla_car.model)
+tesla_car.run()
+tesla_car.auto_run()
+
