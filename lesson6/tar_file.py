881c566446124b83230e9b38803f83efca550a68
Sumio K <tap.miwo@gmail.com>
2022-12-09 20:15:56 +0900

    tar

--- lesson6/tar_file.py
+++ lesson6/tar_file.py
@@ -4,4 +4,6 @@ import tarfile
 #   tr.add('test_dir')
 
 with tarfile.open('test.tar.gz', 'r:gz') as tr:
-  tr.extractall(path='test_tar')
+  # tr.extractall(path='test_tar')
+  with tr.extractfile('test_dir/test.txt')as f:
+    print(f.read())
