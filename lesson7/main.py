e8c9700c67136368b5b23614863971b08d7c991f
Sumio K <tap.miwo@gmail.com>
2022-12-12 06:43:51 +0900

    error such file not exist

--- lesson7/main.py
+++ lesson7/main.py
@@ -1,7 +1,5 @@
 import csv
 
-restaurants = {}
-
 # 挨拶、名前を聞く
 print('=========================================================')
 print('こんにちは!私はRobokoです。あなたの名前を教えてください')
@@ -11,20 +9,23 @@ name = input('名前:')
 # 名前を変数に代入し、好きなレストランを聞く
 print(f'{name}さん。どこのレストランが好きですか?')
 
-# レストランの名前を変数に保存する
+# レストランの名前を変数rに保存する
 r = input('入力:')
-restaurants.append(r)
-print(restaurants)
 
-# レストランの名前と回数をcsvファイルへ出力する
-with open('roboter.csv', 'w', newline='', encoding='utf-8') as csv_file:
-  fieldnames = ['Restaurant', 'Count']
-  writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-  writer.writeheader()
-  writer.writerow({'Restaurant': r, 'Count': 1})
-  
+# csvファイルを開き、中のデータを辞書restaurantsに代入
+restaurants = {}
 
-with open('roboter.csv', 'r') as csv_file:
+with open('roboter.csv', 'r+') as csv_file:
   reader = csv.DictReader(csv_file)
   for row in reader:
     restaurants.append({row['Restaurant']: row['Count']})
+# もし既に存在しているレストランだったらCountを+1する
+  if restaurants[r]:
+    row['Count'] += 1
+# 存在していない場合、Countが１の状態で新しく作成する
+  else:
+    fieldnames = ['Restaurant', 'Count']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow({'Restaurant': r, 'Count': 1})
+
